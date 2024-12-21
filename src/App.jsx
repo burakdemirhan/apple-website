aa551953204487f4d90334e3a5fcc5425140c19c
burakdemirhan <burkidad@outlook.com>
2024-12-21 19:39:21 +0300

    Add initial components: Navbar, Highlights, and Hero sections

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Hero from "./components/Hero";
+import Highligths from "./components/Highligths";
+import Navbar from "./components/Navbar";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Highligths />
+    </>
+  );
+}
+
+export default App;
